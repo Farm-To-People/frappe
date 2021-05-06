26ff84225d5979a17d37b7b586d3b6413e0283ad
Brian Pond <brian@pondconsulting.net>
2021-05-05 18:03:11 -0700

    Improve Email Domains

--- frappe/email/doctype/email_domain/email_domain.js
+++ frappe/email/doctype/email_domain/email_domain.js
@@ -5,6 +5,12 @@ frappe.ui.form.on("Email Domain", {
 	},
 
 	refresh:function(frm){
+		// Farm To People
+		frm.add_custom_button(__("Validate Email Settings"), () => {
+			validate_domain(cur_frm, cur_frm.doc);
+		});
+		// EOM
+
 		if (frm.doc.email_id){
 			frm.set_value("domain_name",frm.doc.email_id.split("@")[1])
 		}
@@ -27,3 +33,19 @@ frappe.ui.form.on("Email Domain", {
 		}
 	}
 })
+
+function validate_domain (caller_frm, doc) {
+	// Validate the email domain's settings
+	frappe.call({
+		method: 'frappe.email.doctype.email_domain.email_domain.validate_domain',
+		args: { 'email_domain_name': doc.name },  // Warning: Python function must have same argument name.
+		callback: function(r) {
+            if (r.message) {
+				// Once complete, do this:
+				frappe.msgprint(__(r.message));
+				caller_frm.reload_doc();
+			}
+		}
+    });
+}
+	
--- frappe/email/doctype/email_domain/email_domain.py
+++ frappe/email/doctype/email_domain/email_domain.py
@@ -3,11 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import imaplib,poplib,smtplib
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils import validate_email_address ,cint, cstr
-import imaplib,poplib,smtplib
+
 from frappe.email.utils import get_port
 
 class EmailDomain(Document):
@@ -15,7 +17,10 @@ class EmailDomain(Document):
 		if self.domain_name:
 			self.name = self.domain_name
 
-	def validate(self):
+	# FTP: Removing the validate function.  It prevents saving the Domain before online
+	# validation, which makes it awkward for correcting or debugging just 1 or 2 fields.
+
+	def DEL_validate(self):
 		"""Validate email id and check POP3/IMAP and SMTP connections is enabled."""
 		if self.email_id:
 			validate_email_address(self.email_id, True)
@@ -74,7 +79,70 @@ class EmailDomain(Document):
 				email_account.set("smtp_server",self.smtp_server)
 				email_account.set("smtp_port",self.smtp_port)
 				email_account.save()
-			except Exception as e:
+			except Exception as ex:
 				frappe.msgprint(email_account.name)
-				frappe.throw(e)
+				frappe.throw(ex)
 				return None
+
+# Spectrum Fruits
+@frappe.whitelist()
+def validate_domain(email_domain_name):
+	"""Validate email id and check POP3/IMAP and SMTP connections is enabled."""
+
+	doc = frappe.get_doc("Email Domain",email_domain_name)
+	if not doc:
+		frappe.throw(_(f"Could not find document 'Email Domain' named '{email_domain_name}'"))
+
+	if doc.email_id:
+		validate_email_address(doc.email_id, True)
+
+	if frappe.local.flags.in_patch:
+		frappe.msgprint(_("Skipping domain tests because mode = 'in patch'"))
+		return
+
+	if frappe.local.flags.in_test:
+		frappe.msgprint(_("Skipping domain tests because mode = 'in test'"))
+		return
+
+	if frappe.local.flags.in_install:
+		frappe.msgprint(_("Skipping domain tests because mode = 'in install'"))
+		return
+
+	# Inbound Email (IMAP or POP3)
+	try:
+		if doc.use_imap:
+			# IMAP
+			if doc.use_ssl:
+				test = imaplib.IMAP4_SSL(doc.email_server, port=get_port(doc))
+			else:
+				test = imaplib.IMAP4(doc.email_server, port=get_port(doc))
+		else:
+			# POP3
+			if doc.use_ssl:
+				test = poplib.POP3_SSL(doc.email_server, port=get_port(doc))
+			else:
+				test = poplib.POP3(doc.email_server, port=get_port(doc))
+	except Exception:
+		frappe.throw(_("Incoming email account not correct"))
+		return None
+	finally:
+		try:
+			if doc.use_imap:
+				test.logout()
+			else:
+				test.quit()
+		except Exception:
+			pass
+
+	# Outbound Email (SMTP)
+	try:
+		if doc.use_tls and not doc.smtp_port:
+			doc.smtp_port = 587
+			doc.save()
+		sess = smtplib.SMTP(cstr(doc.smtp_server or ""), cint(doc.smtp_port) or None)
+		sess.quit()
+	except Exception:
+		frappe.throw(_("Outgoing email account not correct"))
+		return None
+
+	frappe.msgprint(_("\u2713 Email domain '{0}' is valid.".format(email_domain_name)), indicator='green')
